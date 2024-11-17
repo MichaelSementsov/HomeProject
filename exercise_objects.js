d9a6bc590e85f4bbd6acd22c4040831321366250
MichaelSementsov <mishasementsov@gmail.com>
2024-11-17 11:23:41 +0300

    obj.problems

--- exercise_objects.js
+++ exercise_objects.js
@@ -76,3 +76,66 @@
 
 
 //object.method(callback, 1, 2);      //3
+
+
+//let user = {};
+//user.name = 'John';
+//user.surname = 'Smith';
+//user.name = 'Pete';
+//delete user.name;
+
+//console.log(user);
+
+//}
+
+
+// let schedule = {};
+// alert( isEmpty(schedule));   //true
+// schedule["8:30"] = "get up";
+// alert( isEmpty(schedule));  // false
+//
+// function isEmpty(obj) {
+//     for (let key in obj){
+//         return false;
+//     }
+//     return true;
+// }
+
+
+
+// const user ={
+//     name: "Jonh"
+// };
+//
+// console.log(user);
+//
+
+
+// let salaries = {
+//     John: 100,
+//     Ann:  160,
+//     Pete: 130
+// };
+//
+// let sum = 0;
+// for (let key in salaries ){
+//     sum += salaries[key];
+// }
+// console.log(sum);
+
+
+
+let menu = {
+    width: 200,
+    height:300,
+    title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] == 'number') {
+            obj[key] *= 2;
+        }
+    }
+}
+
